1c71261be5be1f2b6ba19b11664284eaa5c3a746
ANSraju <25mca04@mgits.ac.in>
2026-01-02 11:57:46 +0530

    Update pg14.php

--- pg14.php
+++ pg14.php
@@ -1,23 +1,15 @@
-<?php
-
-$cricketers = ["Sachin Tendulkar", "Virat Kohli", "MS Dhoni", "Brian Lara", "Jacques Kallis"];
-?>
-<html>
-<head>
-    <title>Cricketers Array</title>
-</head>
+<!DOCTYPE html>
+<html lang="en">
 <body>
-<table border="1" cellpadding="10" cellspacing="0">
-    <tr><th>Si No.</th>
-    <th>Cricketers Name</th></tr>
-    <?php
-    $count=1;
-    foreach ($cricketers as $cricketer) {
-        echo "<tr><td>$count</td>";
-        echo "<td>$cricketer</td></tr>";
-        $count++;
-    }
-    ?>
-</table>
+<?php
+$cricket_players = array("Sachin Tendulkar", "Virat Kohli", "MS Dhoni", "Rohit Sharma");
+$count = 1;?>
+  <table border="1">
+    <tr> <th> SI_NO </th> <th> PLAYERS NAME </th> </tr>
+    <?php foreach ($cricket_players as $player) {?>
+    <tr> <td> <?php echo $count; ?> </td> <td> <?php echo $player; ?> </td> </tr> <?php
+    $count++;
+}
+?>    
 </body>
 </html>
